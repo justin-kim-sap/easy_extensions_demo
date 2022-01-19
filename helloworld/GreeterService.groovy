48f55a5e8402490cab997497c9c115561ae8552b
Yannick Robin <yannick.robin@sap.com>
2022-01-19 18:00:04 +0800

    Add GreeterService

---
+++ helloworld/GreeterService.groovy
@@ -0,0 +1,11 @@
+import Dependency
+
+class GreeterService {
+
+    def spring;
+
+    String sayHello(firstName) {
+        def greet = new Dependency().message + firstName;
+        greet
+    }
+}
