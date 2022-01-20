8e05105c3bff46ff900b64a626609f5e103d3110
Yannick Robin <yannick.robin@sap.com>
2022-01-20 15:17:14 +0800

    a

--- helloworld/GreeterService.groovy
+++ helloworld/GreeterService.groovy
@@ -1,11 +1,11 @@
-import Dependency
+//import Dependency
 
 class GreeterService {
 
     def spring;
 
     String sayHello(firstName) {
-        def greet = new Dependency().message + firstName;
-        greet
+        //def greet = new Dependency().message + firstName;
+        "hello " + firstName;
     }
 }
