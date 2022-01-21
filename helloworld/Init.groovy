8bc61fd600df17ee0cfcddee3e3b34c0c3775530
Yannick Robin <yannick.robin@sap.com>
2022-01-21 21:49:05 +0800

    a

--- helloworld/Init.groovy
+++ helloworld/Init.groovy
@@ -4,17 +4,14 @@ import org.springframework.beans.factory.groovy.GroovyBeanDefinitionReader
 void registerBeans() {
     LOG.info('Register Spring beans');
 
-    //def reader = new GroovyBeanDefinitionReader(spring.getBeanFactory())
     def reader = new GroovyBeanDefinitionReader(spring.getBeanFactory())
-
-
-    /*
+    
     reader.beans {
         greeterService(GreeterService)
         easyTestBeanService(MyEasyTestBeanService)
     }
-    */
-    reader.loadBeanDefinitions("file:/Users/i303937/Documents/app/hybris1905/hybris/temp/hybris/easy_extensions/helloworld/EasyBeans.groovy");
+    
+    //reader.loadBeanDefinitions("file:/Users/i303937/Documents/app/hybris1905/hybris/temp/hybris/easy_extensions/helloworld/EasyBeans.groovy");
 }
 
 LOG = LoggerFactory.getLogger("easy_helloworld");
