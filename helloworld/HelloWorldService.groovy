bac449c9f5f284780edb251d5b643d926b206293
Yannick Robin <yannick.robin@sap.com>
2022-01-21 11:24:23 +0800

    a

--- helloworld/HelloWorldService.groovy
+++ helloworld/HelloWorldService.groovy
@@ -2,7 +2,7 @@ class HelloWorldService {
 
     def spring;
 
-    String sayHello(firstName) {
+    String sayHello(String firstName) {
         def message = new GreeterMessage().message + firstName + " from HelloWorldService";
         return message;
     }
