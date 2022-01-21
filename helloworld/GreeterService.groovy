e0db5104f024688d1d8c5384f84b4db051281c1c
Yannick Robin <yannick.robin@sap.com>
2022-01-21 10:26:45 +0800

    a

--- helloworld/GreeterService.groovy
+++ helloworld/GreeterService.groovy
@@ -6,7 +6,7 @@ class GreeterService {
 
     String sayHello(firstName) {
         //def message = new Dependency().message + firstName;
-        def message = "hello " + firstName "from GreeterService";
+        def message = "hello " + firstName + " from GreeterService";
         println(message);
         return message;
     }
