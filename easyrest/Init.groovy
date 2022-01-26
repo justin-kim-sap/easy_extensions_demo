37912c8063356192279713e938f853fcdb1e6002
Yannick Robin <yannick.robin@sap.com>
2022-01-27 01:07:24 +0800

    a

---
+++ easyrest/Init.groovy
@@ -0,0 +1,15 @@
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.groovy.GroovyBeanDefinitionReader
+
+void registerBeans() {
+    LOG.info('Register beans');
+
+    def reader = new GroovyBeanDefinitionReader(spring.getBeanFactory())
+    reader.beans {
+        }
+    }
+}
+
+LOG = LoggerFactory.getLogger("easyrest");
+LOG.info("Initializing...");
+registerBeans();
