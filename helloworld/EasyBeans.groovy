bd99dbf4a39adf9d2a4c6d00ffb9148e6cb9d263
Yannick Robin <yannick.robin@sap.com>
2022-02-01 18:09:44 +0800

    a

--- helloworld/EasyBeans.groovy
+++ helloworld/EasyBeans.groovy
@@ -1,4 +1,24 @@
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.groovy.GroovyBeanDefinitionReader
+
+LOG = LoggerFactory.getLogger("easy_helloworld");
+LOG.info('Register Spring beans');
+
+def reader = new GroovyBeanDefinitionReader(spring.getBeanFactory())
+    
+reader.beans {
+    greeterService(GreeterService) {
+        spring = spring
+    }
+    easyTestBeanService(MyEasyTestBeanService)
+    priceService(EasyPriceService) {
+        spring = spring
+    }
+}
+
+/*
 beans {
     greeterService(GreeterService) {
     }
 }
+*/
