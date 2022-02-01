1d66cf66f85b47a330423a6cb12852515962dcd1
Yannick Robin <yannick.robin@sap.com>
2022-02-02 00:44:56 +0800

    a

--- flexiblesearch/FlexibleSearchDemoService.groovy
+++ flexiblesearch/FlexibleSearchDemoService.groovy
@@ -3,15 +3,9 @@ class FlexibleSearchDemoService {
     def spring;
 
     int getProductTotalCount() {
-        def catalogVersionService = spring.getBean("catalogVersionService");
-        def flexibleSearchService = spring.getBean("flexibleSearchService");
-
-        def catVersion = catalogVersionService.getCatalogVersion("Default", "Online");
-        catalogVersionService.setSessionCatalogVersions(java.util.Collections.singleton(catVersion));
-
         def query = "SELECT {pk} FROM {Product}";
         def searchResult = flexibleSearchService.search(query);
-        print("Total number of products: " + searchResult.getTotalCount());
+        println("Total number of products: " + searchResult.getTotalCount());
 
         searchResult.getTotalCount();
     }
