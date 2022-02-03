73b0f6b4f2e2dc9ffbc0983bf41cbf753317fa18
Yannick Robin <yannick.robin@sap.com>
2022-02-04 07:39:32 +0800

    a

--- flexiblesearch/EasyBeans.groovy
+++ flexiblesearch/EasyBeans.groovy
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.groovy.GroovyBeanDefinitionReader
 LOG = LoggerFactory.getLogger("flexiblesearch");
 LOG.debug('Register Spring beans');
 
+/*
 def reader = new GroovyBeanDefinitionReader(spring.getBeanFactory())
-    
+  
 reader.beans {
     flexibleSearchDemoService(FlexibleSearchDemoService) {
         spring = spring
     }
 }
+*/
