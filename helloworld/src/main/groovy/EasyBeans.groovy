5062147068bad3d005bd5deb40c7a0e8df71e89e
Yannick Robin <yannick.robin@sap.com>
2022-02-05 09:19:31 +0800

    a

---
+++ helloworld/src/main/groovy/EasyBeans.groovy
@@ -0,0 +1,5 @@
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.groovy.GroovyBeanDefinitionReader
+
+LOG = LoggerFactory.getLogger("flexiblesearch");
+LOG.debug('Register Spring beans');
