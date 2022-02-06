93062e51a8db4c274297df61e8614246d20e48fc
Yannick Robin <yannick.robin@sap.com>
2022-02-06 09:50:19 +0800

    a

--- helloworld/src/main/groovy/EasyBeans.groovy
+++ helloworld/src/main/groovy/EasyBeans.groovy
@@ -10,7 +10,6 @@ reader.beans {
     greeterService(GreeterService) {
         spring = spring
     }
-    easyTestBeanService(MyEasyTestBeanService)
     priceService(EasyPriceService) {
         spring = spring
     }
