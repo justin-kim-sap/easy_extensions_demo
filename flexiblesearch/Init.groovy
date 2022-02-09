7b711e28e5263b820c6a0ae1179b554ac3b9d8ab
Yannick Robin <yannick.robin@sap.com>
2022-02-09 15:52:21 +0800

    a

--- flexiblesearch/Init.groovy
+++
@@ -1,5 +0,0 @@
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.groovy.GroovyBeanDefinitionReader
-
-LOG = LoggerFactory.getLogger("easy_flexiblesearch");
-LOG.info("Initializing...");
