0bd9d7af62e8205e2a41b4bd355dd934a93e2d19
Yannick Robin <yannick.robin@sap.com>
2022-02-11 16:00:04 +0800

    a

--- helloworld/src/main/groovy/controller/GreeterController.groovy
+++ helloworld/src/main/groovy/controller/GreeterController.groovy
@@ -2,15 +2,16 @@ package controller;
 
 import com.sap.cx.boosters.easyrest.controller.EasyServiceController;
 import service.GreeterService;
+import java.util.Map;
 
 class GreeterController implements EasyServiceController {
 
     def greeterService;
 
-    def execute(parameters) {
+    Map execute(Map ctx) {
         def response = [:];
         response.'responseCode' = 200;
-        response.'body' = greeterService.sayHello(parameters.firstname);
+        response.'body' = greeterService.sayHello(ctx.parameters.firstname);
         return response
     }
 
