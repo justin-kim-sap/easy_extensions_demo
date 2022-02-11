9d2dcf38dcbb599805607ff250b2e5b25d96006f
Yannick Robin <yannick.robin@sap.com>
2022-02-11 18:58:56 +0800

    a

--- helloworld/src/main/groovy/controller/PriceController.groovy
+++ helloworld/src/main/groovy/controller/PriceController.groovy
@@ -1,37 +1,39 @@
 package controller;
 
-import de.hybris.platform.product.impl.PriceService;
 import com.sap.cx.boosters.easyrest.controller.EasyServiceController;
 import java.util.Map;
+import org.slf4j.LoggerFactory;
 
 class PriceController implements EasyServiceController {
 
-    PriceService priceService;
+    def priceService;
     def catalogVersionService;
     def productService;
+    def LOG = LoggerFactory.getLogger("PriceController");
 
     Map execute(Map ctx) {
         def response = [:];
-        log = org.slf4j.LoggerFactory.getLogger('easyrest');
 
-        catVersion = catalogVersionService.getCatalogVersion('Default', 'Staged');
+        def catVersion = catalogVersionService.getCatalogVersion('Default', 'Staged');
         catalogVersionService.setSessionCatalogVersions(Collections.singleton(catVersion));
 
-        product = productService.getProductForCode(ctx.parameters.code);
-        prices = priceService.getPriceInformationsForProduct(product);
-        log.info('Prices :' + prices);
+        def product = productService.getProductForCode(ctx.parameters.code);
+        def prices = priceService.getPriceInformationsForProduct(product);
+        LOG.info('Prices :' + prices);
 
         response.'responseCode' = 200;
 
-        sBody = '[\n';
-        prices.each{price -> 
-        def priceValue = price.getPrice();
-        def currency = priceValue.getCurrencyIso();
-        def value = priceValue.getValue();
-        sBody += '\t{\n\t\t""value"": ' + value + ',\n\t\t""currency"": ""' + currency + '""\n\t}\n';
+        def sBody = '[\n';
+        prices.each{
+            price -> 
+            def priceValue = price.getPrice();
+            def currency = priceValue.getCurrencyIso();
+            def value = priceValue.getValue();
+            sBody += '\t{\n\t\t"value": ' + value + ',\n\t\t"currency": "' + currency + '"\n\t}\n';
         }
         sBody += ']'
 
+        response.'body' = sBody;
         return response
     }
 
